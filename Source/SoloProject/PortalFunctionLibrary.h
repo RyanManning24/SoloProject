b0968fc403a7de43f7d522c74f5304bfaec572b9
Ryan <ryan2.manning@live.uwe.ac.uk>
2022-04-13 20:26:44 +0100

    Added some code for render targets working properly

---
+++ Source/SoloProject/PortalFunctionLibrary.h
@@ -0,0 +1,21 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "PortalFunctionLibrary.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SOLOPROJECT_API UPortalFunctionLibrary : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+
+public:
+		UFUNCTION(BlueprintCallable, Category = "PortalFunctionLibrary")
+		static void ResizeRenderTarget(UTextureRenderTarget2D* render_target, float size_x, float size_y);
+
+};
